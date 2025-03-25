1aeac09de38647ede3d60f786997ba3d1644d4cb
ujseah <139524995+ujseah@users.noreply.github.com>
2025-03-25 16:30:31 -0400

    UI Canvas Implementation
    Finalized UI text to appear once reaching the door.

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
 
-public class DoorTrigger : MonoBehaviour
+public class HeadsetPromptTrigger : MonoBehaviour
 {
     public GameObject uiCanvas;
 
     void OnTriggerEnter(Collider other)
     {
-        // Check if the object entering the zone has a CharacterController (i.e. is the player)
+        Debug.Log("Something entered the trigger: " + other.name);
+
+        // Check if the object that entered has a CharacterController
         if (other.GetComponent<CharacterController>())
         {
-            uiCanvas.SetActive(true);
+            Debug.Log("Player (with CharacterController) entered the trigger.");
+
+            // Try activating the canvas
+            if (uiCanvas != null)
+            {
+                uiCanvas.SetActive(true);
+                Debug.Log("Canvas should now be active. Active state = " + uiCanvas.activeSelf);
+            }
+            else
+            {
+                Debug.LogWarning("uiCanvas reference is missing in the Inspector!");
+            }
         }
     }
 }
